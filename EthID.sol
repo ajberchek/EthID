5b50bef25f5ca57b87fd45fb8ea8f54a8872904f
ajberchek <ajberchek@gmail.com>
2017-09-28 20:11:15 -0700

    Remove ID has been implemented

--- EthID.sol
+++ EthID.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.4.4;
 contract EthID
 {
     mapping (address => string) public EIDs;
-    mapping (address => address[]) EIDsToAdd;
-    mapping (address => address[]) EIDsToRemove;
+    mapping (address => address[]) public EIDsToAdd;
+    mapping (address => address[]) public EIDsToRemove;
 
     mapping (address => address[]) public trustedEIDManagers;
 
     address[] public quorum;
 
-    function EthID(address[] toBeQuorum) public 
+    function EthID(address[] toBeQuorum) public
     {
         quorum = toBeQuorum;
     }
@@ -31,6 +31,8 @@ contract EthID
         require(present(quorum, addr) == true);
     }
 
+    //TODO fix exploit:
+    //Last signer can change name arbitrarily, store name as well as signers
     function addEID(address addr, string name) isQuorumMember(msg.sender)
     {
         //Ensure the address we are suggesting isnt already an EID
@@ -58,20 +60,49 @@ contract EthID
         }
     }
 
+    function setTrustedEID(address[] addrs)
+    {
+        if(EIDs[msg.sender] != 0 && trustedEIDManagers[msg.sender] == 0)
+        {
+            trustedEIDManagers[msg.sender] = addrs;
+        }
+    }
+
     function removeEID(address addr)
     {
         //Get addr's trustedEIDManagers, make sure the sender is in that list, and 
         //increment the number of people voting to remove and if it is majority then remove
+        if(EIDs[addr] != 0)
+        {
+            address[] remSigs = EIDsToRemove[addr];
+            address[] trusted = trustedEIDManagers[addr];
+            if(present(trusted,msg.sender) == true && present(remSigs,msg.sender) == false)
+            {
+                remSigs.push(msg.sender);
+                if(2*remSigs.length > trusted.length)
+                {
+                    //We have reached majority, add the addr and delete
+                    //this suggested list
+                    delete EIDs[addr];
+                    delete EIDsToRemove[addr];
+                }
+                else
+                {
+                    //Reset the map to have this larger list of signatures
+                    EIDsToRemove[addr] = remSigs;
+                }
+            }
+        }
     }
 
     function getAddr(string name) constant returns (address)
     {
         //return the address associated with the name and -1 if none
-        for (uint8 i = 0; i < EIDs.length; i++) 
+        for (uint8 i = 0; i < EIDs.length; i++)
         {
             if (EIDs[i] == name) { return EIDs[i]; }
         }
-        
+
         return address(0);
     }
 
