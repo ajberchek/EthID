9be48df497e121a82c2bf957a110da60f2257eec
Andrew (anoa) <anoa@openmailbox.org>
2017-09-28 18:54:09 -0700

    Minor cleanup

--- EthID.sol
+++ EthID.sol
@@ -10,19 +10,25 @@ contract EthID
 
     address[] public quorum;
 
-    function EthID(address[] toBeQuorum) public {
+    function EthID(address[] toBeQuorum) public 
+    {
         quorum = toBeQuorum;
     }
 
     function present(address[] addrSet, address addr) constant returns (bool)
     {
         // Return whether address was present
-        return addrSet[addr].isValue;
+        for (uint8 i = 0; i < addrSet.length; i++) 
+        {
+            if (addrSet[i] == addr) { return true; }
+        }
+
+        return false;
     }
 
     modifier isQuorumMember(address addr)
     {
-        require(present(quorum,addr) == true);
+        require(present(quorum, addr) == true);
     }
 
     function addEID(address addr, string name) isQuorumMember(msg.sender)
@@ -30,7 +36,7 @@ contract EthID
         if(getAddr(name) != address(0))
         {
             address[] signatures = EIDsToAdd[addr];
-            if(present(signatures,msg.sender) == false)
+            if(present(signatures, msg.sender) == false)
             {
                 signatures.push(msg.sender);
                 if(2*signatures.length > quorum.length)
@@ -55,7 +61,8 @@ contract EthID
     function getAddr(string name) constant returns (address)
     {
         //return the address associated with the name and -1 if none
-        for (uint8 i = 0; i < EIDs.length; i++) {
+        for (uint8 i = 0; i < EIDs.length; i++) 
+        {
             if (EIDs[i] == name) { return true; }
         }
         
