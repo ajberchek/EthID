06b2f4df6803784a0aafe8a44199cc77cfb9eaa3
ajberchek <ajberchek@gmail.com>
2017-09-28 18:08:52 -0700

    Created declarations for the necessary functions

--- EthID.sol
+++ EthID.sol
@@ -10,4 +10,31 @@ contract EthID
         quorum = toBeQuorum;
     }
 
+    function present(address[] addrSet, address addr) constant returns (bool)
+    {
+        //For loop to check presence of addr in addrSet
+    }
+
+    modifier isQuorumMember(address addr)
+    {
+        require(present(quorum,addr) == true);
+    }
+
+    function addEID(address addr, string name) isQuorumMember(msg.sender)
+    {
+        //Add to a struct the count of quorum members voting to add someone
+        //if count is majority then add the EID
+    }
+
+    function removeEID(address addr)
+    {
+        //Get addr's trustedEIDManagers, make sure the sender is in that list, and 
+        //increment the number of people voting to remove and if it is majority then remove
+    }
+
+    function getAddr(string name)
+    {
+        //return the address associated with the name and -1 if none
+    }
+
 }
