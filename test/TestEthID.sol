b41174f25fc23ca62e61f8a137517efecd64ae06
Andrew Morgan <andrew@amorgan.xyz>
2017-09-28 22:18:03 -0700

    Add build folder.

--- test/TestEthID.sol
+++ test/TestEthID.sol
@@ -10,9 +10,11 @@ contract TestEthID {
 		EthID eid = EthID(DeployedAddresses.EthID());
 
 		address andrewAddr = address(0x884025c566d2f940363381ddded0a16838a5feac);
+		address badAddr = address(0x1111111111111111111111111111111111111111);
 
 		eid.addEID(andrewAddr, "andrew");
 
 		Assert.equal(eid.getAddr("andrew"), andrewAddr, "Should be able to add address and get it back later.");
+		Assert.notEqual(eid.getAddr("andrew"), badAddr, "Shouldn't match bad address.");
 	}
 }
